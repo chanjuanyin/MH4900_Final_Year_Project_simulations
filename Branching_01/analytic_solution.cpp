b36ceb7fc7fe81dbae1fe691b49086f392fb5718
chanjuanyin <chanjuanyin@outlook.com>
2023-12-04 21:50:33 +0800

    Slight improvement on Branching_01

--- Branching_01/analytic_solution.cpp
+++ Branching_01/analytic_solution.cpp
@@ -27,13 +27,13 @@ xt::xarray<double> fix_t_varies_x(double t, double a, double v, double lower_bou
     xt::xarray<double> arr = xt::arange(lower_bound, upper_bound + delta_x, delta_x);
     double double_1 = v * t;
     xt::xarray<double> factor_1 = {double_1};
-    xt::xarray<double> arr2 = 6. / ( (arr + factor_1) * (arr + factor_1) );
+    xt::xarray<double> arr2 = a * 6. / ( (arr + factor_1) * (arr + factor_1) );
     return arr2;
 }
 
 xt::xarray<double> fix_x_varies_t(double x, double a, double v, double lower_bound, double upper_bound, double delta_t_) {
     xt::xarray<double> arr = xt::arange(lower_bound, upper_bound + delta_t_, delta_t_);
-    xt::xarray<double> arr2 = 6. / ( (x + v * arr) * (x + v * arr) );
+    xt::xarray<double> arr2 = a * 6. / ( (x + v * arr) * (x + v * arr) );
     return arr2;
 } 
 
@@ -47,11 +47,11 @@ int main()
         double t = static_cast<double> (i);
         xt::xarray<double> arr;
         if (i==0) {
-            arr = fix_t_varies_x(t, 0.5, sqrt(2), 1., 10., 0.01);
+            arr = fix_t_varies_x(t, 1., sqrt(2), 1., 10., 0.01);
             arr = arr.reshape({1, static_cast<int> ((10 - 1)/0.01) + 1});
         }
         else {
-            arr = fix_t_varies_x(t, 0.5, sqrt(2), 0., 10., 0.01);
+            arr = fix_t_varies_x(t, 1., sqrt(2), 0., 10., 0.01);
             arr = arr.reshape({1, static_cast<int> ((10 - 0)/0.01) + 1});
         }
         string file_name = "../Branching_01/output/t_equals_";
@@ -65,11 +65,11 @@ int main()
         double x = static_cast<double> (i);
         xt::xarray<double> arr;
         if (i==0) {
-            arr = fix_x_varies_t(x, 0.3, sqrt(2), 1., 10., 0.01);
+            arr = fix_x_varies_t(x, 1., sqrt(2), 1., 10., 0.01);
             arr = arr.reshape({1, static_cast<int> ((10 - 1)/0.01) + 1});
         }
         else {
-            arr = fix_x_varies_t(x, 0.3, sqrt(2), 0., 10., 0.01);
+            arr = fix_x_varies_t(x, 1., sqrt(2), 0., 10., 0.01);
             arr = arr.reshape({1, static_cast<int> ((10 - 0)/0.01) + 1});
         }
         string file_name = "../Branching_01/output/x_equals_";
